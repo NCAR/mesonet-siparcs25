cfabddff5a28b3a6f819cb97ae974f247c8018f3
Adebowale <adadelek11@gmail.com>
2025-07-10 14:23:28 -0600

    code fixes

--- edge-servers/deploy.sh
+++ edge-servers/deploy.sh
@@ -1,6 +1,6 @@
 # Make script executable
 chmod +x /home/ncar/mesonet-siparcs25/edge-servers/setup_and_run.sh
-
+sudo cp lora-gateway.service /etc/systemd/system/lora-gateway.service
 # Install service
 sudo systemctl daemon-reload
 sudo systemctl enable lora-gateway
