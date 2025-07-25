b64538ae6884b245287ed1720f1c269856f70a65
oppongbaah <oppongbaahisaacmega@gmail.com>
2025-07-24 19:30:08 -0600

    feat: change credit base image

--- cloud/services/credit/scripts/credit.sh
+++ cloud/services/credit/scripts/credit.sh
@@ -3,6 +3,7 @@
 # Fall back if env vars are not provided
 # Set timezone for Denver
 TZ=America/Denver
+DATE=$(TZ=$TZ date '+%Y-%m-%d %H:%M:%S')
 
 # Fallback if environment vars are not provided
 ENV_NAME=${ENV_NAME:-credit}
@@ -10,9 +11,13 @@ START_DATE=${START_DATE:-$(TZ=$TZ date +"%Y-%m-%d 00:00:00")}
 END_DATE=${END_DATE:-$(TZ=$TZ date -d "+1 day" +"%Y-%m-%d 00:00:00")}
 SAVE_LOC=${SAVE_LOC:-"credit_run/results/gfs_init_$(TZ=$TZ date +%Y%m%d_0000).zarr"}
 
-echo "[$(date)] Starting Forecast Pipeline in the environment: $ENV_NAME"
-echo "[$(date)] Forecast window: $START_DATE → $END_DATE"
-echo "[$(date)] Output path: $SAVE_LOC"
+echo "[$DATE] Starting Forecast Pipeline in the environment: $ENV_NAME"
+echo "[$DATE] Forecast window: $START_DATE → $END_DATE"
+echo "[$DATE] Output path: $SAVE_LOC"
+
+# Test GPU availability
+echo "[$DATE] Testing GPU availability..."
+conda run -n "$ENV_NAME" python -c "import torch; print('CUDA available?', torch.cuda.is_available())"
 
 # Modify model.yml using sed
 echo "Updating model.yml..."
@@ -21,23 +26,24 @@ sed -i "s|^ *forecast_end_time:.*|        forecast_end_time: \"$END_DATE\"|" mod
 sed -i "s|^\([[:space:]]*save_loc:\).*zarr\"$|\1 \"$SAVE_PATH\"|" model.yml
 
 # Start pipeline
-echo "[$(date)] Running gfs_init.py..."
+echo "[$DATE] Running gfs_init.py..."
 conda run -n "$ENV_NAME" python miles-credit/applications/gfs_init.py -c model.yml
 
 if [ $? -eq 0 ]; then
-    echo "[$(date)] gfs_init.py completed successfully."
+    echo "[$DATE] gfs_init.py completed successfully."
 
-    echo "[$(date)] Running rollout_realtime.py..."
+    echo "[$DATE] Running rollout_realtime.py..."
     conda run -n "$ENV_NAME" python miles-credit/applications/rollout_realtime.py -c model.yml
 
     if [ $? -eq 0 ]; then
-        echo "[$(date)] Forecast complete. Transforming..."
+        echo "[$DATE] Forecast complete. Transforming..."
         conda run -n "$ENV_NAME" python services/credit/transform_forecast_output.py
+        echo "[$DATE] Credit is running fine ..."
     else
-        echo "[$(date)] Forecast failed."
+        echo "[$DATE] Forecast failed."
     fi
 else
-    echo "[$(date)] gfs_init.py failed."
+    echo "[$DATE] gfs_init.py failed."
 fi
 
-echo "[$(date)] Done."
+echo "[$DATE] Done."
