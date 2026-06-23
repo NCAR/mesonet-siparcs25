0c1dea02e174c402ed517dc6a22e18adb6266815
Pouya Shaeri <pshaeri@ucar.edu>
2026-06-23 13:26:41 -0600

    reverted back to SF7

--- edge-servers/pi_lora.py
+++ edge-servers/pi_lora.py
@@ -49,10 +49,10 @@ def initialize_radio(freq=915.0, power=23):
         rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, freq)
         rfm9x.tx_power = power
 
-        # rfm9x.spreading_factor = 7
-        rfm9x.spreading_factor = 11 # avoid collision with Nathan's
+        rfm9x.spreading_factor = 7
+        # rfm9x.spreading_factor = 11 # avoid collision with Nathan's
 
-        rfm9x.low_datarate_optimize = True
+        # rfm9x.low_datarate_optimize = True
         
         rfm9x.signal_bandwidth = 125000
         rfm9x.coding_rate = 5
